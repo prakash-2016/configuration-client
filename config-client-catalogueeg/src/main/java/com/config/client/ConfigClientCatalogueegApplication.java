bbfb630e2e9f01154b7917347a3ac8859535cdc2
prakash-2016 <prakash.chandra.mailbox@gmail.com>
2017-04-17 16:58:13 +0530

    client for config server
    client for config server

---
+++ config-client-catalogueeg/src/main/java/com/config/client/ConfigClientCatalogueegApplication.java
@@ -0,0 +1,12 @@
+package com.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientCatalogueegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientCatalogueegApplication.class, args);
+	}
+}
